6b52d094817aa55fd954de6c3b2ff6a89cfc39e4
akimkhabibullin <akim.khabibullin@outlook.com>
2024-10-26 17:38:19 -0700

    Update java.js

--- m05/java.js
+++ m05/java.js
@@ -1,7 +1,6 @@
-var table = 3;             // Unit of table
-
-operator = GetValueFromUser("operator");
-table = GetValueFromUser("table")
+var table = 3; // Unit of table
+var operator = GetValueFromUser("operator"); // Retrieve the operator
+table = parseInt(GetValueFromUser("table"), 10); // Retrieve and convert the table value to an integer
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
@@ -9,31 +8,38 @@ el.innerHTML = GetTableContent(operator, table);
 
 function GetValueFromUser(valueType) {
   var greetingMessage = 'Hello. How are you?';
-  if (valueType == "operator") {
-    greetingMessage += " Enter addition or multiplication";
+  if (valueType === "operator") {
+    greetingMessage += " Enter 'addition' or 'multiplication'";
+  } else {
+    greetingMessage += " Enter number";
   }
-  else
-  {
-    greetingMessage += " Enter table";
+  
+  var userInput = prompt(greetingMessage);
+  if (userInput === null) {
+    return ''; // Exit if the user cancels
   }
-  return prompt(greetingMessage);
+  return userInput.trim(); // Remove extra whitespace
 }
 
 function GetTableContent(operator, table) {
-  var i = 1;                 // Set counter to 1
-  var msg = '';              // Message  
+  var i = 1; // Set counter to 1
+  var msg = ''; // Message  
+  
   if (operator === 'addition') {
     // Do addition
     while (i < 11) {
       msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
       i++;
     }
-  } else {
+  } else if (operator === 'multiplication') {
     // Do multiplication
     while (i < 11) {
       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
     }
+  } else {
+    msg = "Invalid operator entered.";
   }
+  
   return msg;
 }
