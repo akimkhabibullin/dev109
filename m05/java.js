ca0370468ffa565f467be96e845c7cff5c683331
akimkhabibullin <akim.khabibullin@outlook.com>
2024-10-30 21:51:11 -0700

    Update java.js

--- m05/java.js
+++ m05/java.js
@@ -1,40 +1,37 @@
-// Define initial variables
-var table = parseInt(GetValueFromUser("table"), 10);
 var operator = GetValueFromUser("operator");
+var table = parseInt(GetValueFromUser("table"), 10);  // Convert input to integer
 
-// Get the HTML element to display output
-var el = document.getElementById("blackboard");
+// Write the message into the page
+var el = document.getElementById('blackboard');
 el.innerHTML = GetTableContent(operator, table);
 
-// Function to get user input
 function GetValueFromUser(valueType) {
-    let greetingMessage = "Hello. How are you?";
-    if (valueType === "operator") {
-        greetingMessage += " Enter 'addition' or 'multiplication'";
-    } else {
-        greetingMessage += " Enter a number";
-    }
-
-    let userInput = prompt(greetingMessage);
-    if (userInput === null) {
-        return ''; // Return empty string if user canceled the prompt
-    }
-    return userInput.trim();
+  var greetingMessage = 'Hello. How are you? ';
+  if (valueType === "operator") {
+    greetingMessage += "Enter 'addition' or 'multiplication'";
+  } else {
+    greetingMessage += "Enter a number";
+  }
+  return prompt(greetingMessage);
 }
 
-// Function to generate table content
 function GetTableContent(operator, table) {
-    let msg = ""; // String to store message
-    if (operator === "addition") {
-        for (let i = 1; i <= 10; i++) {
-            msg += i + " + " + table + " = " + (i + table) + "<br />";
-        }
-    } else if (operator === "multiplication") {
-        for (let i = 1; i <= 10; i++) {
-            msg += i + " x " + table + " = " + (i * table) + "<br />";
-        }
-    } else {
-        msg = "Invalid operator entered.";
+  var i = 1;                 // Set counter to 1
+  var msg = '';              // Message  
+  if (operator === 'addition') {
+    // Do addition
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
     }
-    return msg;
+  } else if (operator === 'multiplication') {
+    // Do multiplication
+    while (i < 11) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
+    }
+  } else {
+    msg = "Invalid operator entered.";
+  }
+  return msg;
 }
