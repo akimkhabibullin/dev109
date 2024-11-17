0215b4da6ccbe28623a0635159c11c57cd504fd8
akimkhabibullin <akim.khabibullin@outlook.com>
2024-11-16 21:04:12 -0800

    Update java.js

--- m08/javascript/java.js
+++ m08/javascript/java.js
@@ -1,1 +1,13 @@
+function addItem() {
+    var newItemText = document.getElementById('newItem').value;
+
+    var newEl = document.createElement('li');
+
+    var newText = document.createTextNode(newItemText);
+
+    newEl.appendChild(newText);
+
+    var todoList = document.getElementById('todo');
+    todoList.appendChild(newEl);
+}
 
